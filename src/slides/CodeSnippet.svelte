21cd2c75db6e54773522d48b8bca4e4e6e0a2ea6
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2020-09-07 16:55:45 +0200

    added highlightjs to the project, created CodeSnippet component and draft of reactivity slide

---
+++ src/slides/CodeSnippet.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { HighlightSvelte } from 'svelte-highlight'
+  import { javascript } from 'svelte-highlight/languages'
+  import { androidstudio } from 'svelte-highlight/styles'
+
+  export let code
+</script>
+
+<svelte:head>
+  {@html androidstudio}
+</svelte:head>
+
+<div class="codeSnippet">
+  <HighlightSvelte language={javascript} {code} />
+</div>
+
+<style>
+  .codeSnippet {
+    display: flex;
+    text-align: start;
+    justify-content: center;
+    font-size: xx-large;
+  }
+</style>
--- src/slides/Slide1.svelte
+++ src/slides/Slide1.svelte
@@ -1,20 +1,53 @@
 <script>
-  import { onMount, onDestroy } from 'svelte'
+  logMountAndDestroy('Slide 1')
+
   import { logMountAndDestroy } from '../utils.js'
   import SlideContainer from './SlideContainer.svelte'
+  import CodeSnippet from './CodeSnippet.svelte'
 
-  logMountAndDestroy('Slide 1')
+  let codeSnippet1 = `<script>
+  let a = 0;
+  let b = 0;
+  $: c = a + b;
+<\/script>`
+
+  let a = 0
+  let b = 0
+  $: c = a + b
+
+  let codeSnippet2 = `<script>
+  let name = 'Stranger'
+  $: hello = \`Hello, \${name}!\`
+<\/script>`
+
+  let name = 'Stranger'
+  $: hello = `Hello, ${name}!`
 </script>
 
 <SlideContainer>
-  <h1>This is my first slide!</h1>
+  <h1>True reactivity</h1>
 
-  <footer>Test</footer>
+  <div>
+    <CodeSnippet code={codeSnippet1} />
+    a:
+    <input type="number" bind:value={a} />
+    + b:
+    <input type="number" bind:value={b} />
+    = c: {c}
+  </div>
+
+  <div>
+    <CodeSnippet code={codeSnippet2} />
+    <input type="text" bind:value={name} />
+    <br />
+    {hello}
+  </div>
 </SlideContainer>
 
 <style>
   h1 {
-    background-color: brown;
-    align-content: center;
+    display: flex;
+    color: darkorange;
+    justify-content: center;
   }
 </style>
