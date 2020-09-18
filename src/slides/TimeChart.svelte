96845f07b8863b1df733e8965b79bcd5d90e044e
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2020-09-18 12:56:05 +0200

    added time chart slide

---
+++ src/slides/TimeChart.svelte
@@ -0,0 +1,64 @@
+<script>
+  logMountAndDestroy('TimeChart.svelte')
+
+  import { logMountAndDestroy } from '../utils.js'
+  import SlideContainer from '../SlideContainer.svelte'
+  import Pie from 'svelte-chartjs/src/Pie.svelte'
+
+  let data = {
+    datasets: [
+      {
+        data: [12.5, 25, 5, 5, 17.5, 35],
+        backgroundColor: [
+          'mediumaquamarine',
+          'limegreen',
+          'lightcoral',
+          'lightsalmon',
+          'darkkhaki',
+          'chocolate'
+        ]
+      }
+    ],
+    labels: [
+      'Learning Svelte',
+      'Creating the viewer',
+      'Implementing routing',
+      'fighting JS',
+      'Creating the slides',
+      'Cursing at CSS & Flexbox'
+    ]
+  }
+</script>
+
+<SlideContainer>
+  <div class="parent">
+    <p>I spent around 40h on that project</p>
+    <div class="chart">
+      <Pie {data} options={{ maintainAspectRatio: false, responsive: true }} />
+    </div>
+  </div>
+
+</SlideContainer>
+
+<style>
+  .parent {
+    flex: 1 0 auto;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: stretch;
+    font-family: 'Comic Sans MS', cursive, sans-serif;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .chart {
+    flex: 1 0 auto;
+  }
+
+  p {
+    font-style: italic;
+    color: darkolivegreen;
+    text-align: center;
+  }
+</style>
--- src/slides/slidesStore.js
+++ src/slides/slidesStore.js
@@ -7,9 +7,11 @@ import EmptySlide from './EmptySlide.svelte'
 import ComponentSlot from './ComponentSlot.svelte'
 import ButWhy from './ButWhy.svelte'
 import Title from './Title.svelte'
+import TimeChart from './TimeChart.svelte'
 
 const slidesArray = [
   { description: 'Title', slide: Title },
+  { description: 'Time Chart', slide: TimeChart },
   { description: 'True Reactivity', slide: TrueReactivity },
   { description: 'Hello Component Example', slide: HelloComponentExample },
   { description: 'Component Usage', slide: ComponentUsage },
