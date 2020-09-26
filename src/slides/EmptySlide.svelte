b6df381ffbec1d98161117e2369380a1f6016942
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2020-09-26 22:43:31 +0200

    attempt to publish to github page

--- src/slides/EmptySlide.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  logMountAndDestroy('EmptySlide.svelte')
-
-  import { logMountAndDestroy } from '../utils.js'
-  import SlideContainer from '../SlideContainer.svelte'
-</script>
-
-<SlideContainer />
